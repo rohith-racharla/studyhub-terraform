70a98f31886f1755cd304b012ddaf34ee523bff5
Rohith Racharla <rohith.racharla@gmail.com>
2026-07-29 21:57:23 +0530

    update iam permissions

--- iam.tf
+++ iam.tf
@@ -56,29 +56,3 @@ resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
   role       = aws_iam_role.eks_node.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
-
-# Custom read-only policy for Secrets Manager
-resource "aws_iam_policy" "secrets_manager_read_only" {
-  name        = "${var.project_name}-secrets-manager-read-policy"
-  description = "Allows EKS nodes to read secrets from AWS Secrets Manager"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-# Attach custom policy to EKS Node Role
-resource "aws_iam_role_policy_attachment" "secrets_manager_read_attach" {
-  role       = aws_iam_role.eks_node.name
-  policy_arn = aws_iam_policy.secrets_manager_read_only.arn
-}
