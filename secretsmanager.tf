3d6af8db32a8940da72c641337e8247062a027ec
Rohith Racharla <rohith.racharla@gmail.com>
2026-07-30 19:52:20 +0530

    chore: fix secrets manager name and config

--- secretsmanager.tf
+++ secretsmanager.tf
@@ -1,8 +1,10 @@
 resource "aws_secretsmanager_secret" "backend" {
 
-  name = "${var.project_name}/backend-secret"
+  name = "${var.project_name}/backendsecret"
 
   description = "Secrets for StudyHub backend"
+
+  recovery_window_in_days = 7
 
   tags = {
     Name = "${var.project_name}-backend-secret"
