b12cf4110071bb1990a1d5294f59547931c13ce6
Rohith Racharla <rohith.racharla@gmail.com>
2026-09-01 23:25:07 +0530

    add ebs csi driver addon

--- addons.tf
+++ addons.tf
@@ -39,3 +39,13 @@ resource "aws_eks_addon" "pod_identity_agent" {
     aws_eks_node_group.this
   ]
 }
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name = aws_eks_cluster.this.name
+  addon_name   = "aws-ebs-csi-driver"
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  depends_on = [aws_eks_node_group.this]
+}
