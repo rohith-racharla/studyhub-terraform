92d40c2ec6bd9777faabcb622a57fb0af0a9d374
Rohith Racharla <rohith.racharla@gmail.com>
2026-09-07 20:10:09 +0530

    add ebs csi driver iam role and pod identity association

--- pod-identity.tf
+++ pod-identity.tf
@@ -72,3 +72,20 @@ resource "aws_eks_pod_identity_association" "external_secrets" {
 
   role_arn = aws_iam_role.external_secrets.arn
 }
+
+resource "aws_iam_role" "ebs_csi" {
+  name               = "${var.project_name}-ebs-csi-role"
+  assume_role_policy = data.aws_iam_policy_document.pod_identity_trust.json
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  role       = aws_iam_role.ebs_csi.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+resource "aws_eks_pod_identity_association" "ebs_csi" {
+  cluster_name    = aws_eks_cluster.this.name
+  namespace       = "kube-system"
+  service_account = "ebs-csi-controller-sa"
+  role_arn        = aws_iam_role.ebs_csi.arn
+}
